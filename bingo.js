0d27e1c2619f353ab9954e2bbdb13b14a241a695
Rabz92 <rabbiailyas92@gmail.com>
2021-04-22 22:57:03 +0200

    update

--- bingo.js
+++ bingo.js
@@ -1,17 +1,88 @@
 
-const randomNumber = function() {
+// function randomUniqueNum(range, outputCount) {
 
+//     let arr = []
+//     for (let i = 1; i <= range; i++) {
+//       arr.push(i)
+//     }
+  
+//     let result = [];
+  
+//     for (let i = 1; i <= outputCount; i++) {
+//       const random = Math.floor(Math.random() * (range - i));
+//       result.push(arr[random]);
+//       arr[random] = arr[range - i];
+//     }
+  
+//     return result;
+//   }
 
-	let randomized = Math.ceil(Math.random() * 75);
+  const fillArray = function () {
+    const arr = [];
+    for (let i = 0; i < 76; i++) {
+      arr.push(i + 1);
+    }
+    return arr;
+  };
+
+  //this randon number is going to 
+
+  const randomNumber = function(range) {
+
+      // let randomArray = []
+      let randomized = Math.ceil(Math.random() * range.length);
+      let random = range.splice(randomized, 1)[0]
+      return random
+  }
+
+  const createRandom = function(range){
+    let randomNumber = randomNumber(range)
     let randomInputField = document.getElementById('random')
+    randomInputField.value = randomNumber
+    //let randomized = randomUniqueNum(76, 76)
     // let randomInputFieldValue = randomInputField.innerText
-    randomInputField.value = randomized
     let bingoCellNumber = document.getElementsByTagName('h3')
-    for(let i=0; i<bingoCellNumber.length; i++){        
-        if(parseInt(bingoCellNumber[i].innerText) === randomized){
-            bingoCellNumber[i].classList.add('selected')    }
+
+    // if(!randomArray.includes(randomized)){
+    //   randomArray.push(randomized)
+    
+      for(let i=0; i<76; i++){           
+          if(parseInt(bingoCellNumber[i].innerText) === randomNumber){
+            bingoCellNumber[i].classList.add('selected') 
+      }         
+    }
+    console.log(randomized);
+}
+// else{
+//   randomInputField.value =  randomNumber()
+// }
+  
+      
+
+const userBoard = function(){
+
+  let randomArray = []
+    let count = 0
+    let randomNumbers = Math.ceil(Math.random()*76)
+    let bingoCard = document.getElementById('bingo')
+    for(let i=0; i<23; i++){
+        randomArray[i] = randomNumbers
+        count = randomArray[i]
+        // let count = randomNumbers + i
+        let board = document.getElementById('newUserBoard')
+        board.classList.add('board')
+        let boardCells = document.createElement('h4')
+
+        for(let j=0; j<randomArray.length; j++){                   
+          boardCells.innerText = count
+            board.appendChild(boardCells)
+            // bingoCard.appendChild(cell)
+        }
+
 }
 }
+
+
 
 window.onload = function(){
     let bingoCard = document.getElementById('bingo')
@@ -22,6 +93,7 @@ window.onload = function(){
         numbers.innerText = [i]
         cell.appendChild(numbers)
         bingoCard.appendChild(cell)
+        const range = fillArray()
 
         }       
     }
