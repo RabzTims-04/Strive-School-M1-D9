531b241b56ab497e41f138235b537f7855f4788d
Rabz92 <rabbiailyas92@gmail.com>
2021-04-23 07:39:16 +0200

    New code for bingo

--- bingo.js
+++ bingo.js
@@ -1,140 +1,66 @@
-
-// function randomUniqueNum(range, outputCount) {
-
-//     let arr = []
-//     for (let i = 1; i <= range; i++) {
-//       arr.push(i)
-//     }
-  
-//     let result = [];
-  
-//     for (let i = 1; i <= outputCount; i++) {
-//       const random = Math.floor(Math.random() * (range - i));
-//       result.push(arr[random]);
-//       arr[random] = arr[range - i];
-//     }
-  
-//     return result;
-//   }
-
-  const fillArray = function () {
-    const arr = [];
-    for (let i = 0; i < 76; i++) {
-      arr.push(i + 1);
-    }
-    return arr;
-  };
-
-  //this randon number is going to 
-
-  const randomNumber = function(range) {
-
-      // let randomArray = []
-      let randomized = Math.ceil(Math.random() * range.length);
-      let random = range.splice(randomized, 1)[0]
-      return random
+const generateMainBoard = function(){
+  const board = document.querySelector('.main-board')
+  for(let i=0; i<76; i++){
+      board.innerHTML += `<div class = 'cell'>${i+1}</div>`
   }
-
-  const createRandom = function(range){
-    let randomNumber = randomNumber(range)
-    let randomInputField = document.getElementById('random')
-    randomInputField.value = randomNumber
-    //let randomized = randomUniqueNum(76, 76)
-    // let randomInputFieldValue = randomInputField.innerText
-    let bingoCellNumber = document.getElementsByTagName('h3')
-
-    // if(!randomArray.includes(randomized)){
-    //   randomArray.push(randomized)
-    
-      for(let i=0; i<76; i++){           
-          if(parseInt(bingoCellNumber[i].innerText) === randomNumber){
-            bingoCellNumber[i].classList.add('selected') 
-      }         
-    }
-    console.log(randomized);
 }
-// else{
-//   randomInputField.value =  randomNumber()
-// }
+
+const fillArray = function(){
+  const arr=[]
+  for(let i=0; i<76; i++){
+      arr.push(i+1)
+  }
+  return arr
+}
+
+const getRandomNum = function(range){
+  const randIndex = Math.floor(Math.random() * range.length)
+  const random = range.splice(randIndex,1)[0]
+  return random
+}
+
+const generateRandNumber = function(range){
+  const random = getRandomNum(range)
+  const randomNumDiv = document.getElementById('random-value-field')
+  randomNumDiv.value = random
   
-      
+  const cells = document.querySelectorAll('.main-board .cell')
+  cells[random - 1].classList.add('highlight')
 
-const userBoard = function(){
-
-  let randomArray = []
-    let count = 0
-    let randomNumbers = Math.ceil(Math.random()*76)
-    let bingoCard = document.getElementById('bingo')
-    for(let i=0; i<23; i++){
-        randomArray[i] = randomNumbers
-        count = randomArray[i]
-        // let count = randomNumbers + i
-        let board = document.getElementById('newUserBoard')
-        board.classList.add('board')
-        let boardCells = document.createElement('h4')
-
-        for(let j=0; j<randomArray.length; j++){                   
-          boardCells.innerText = count
-            board.appendChild(boardCells)
-            // bingoCard.appendChild(cell)
-        }
-
-}
+  const userCells = document.querySelectorAll('.user-board .cell')
+  userCells.forEach((cell)=>{
+      if(parseInt(cell.innerText) === random){
+          cell.classList.add('highlight')
+      }
+  })
 }
 
-
+const generateUserBoards = function(){
+  const userNumber = document.getElementById('usersNumber').value
+  const container = document.querySelector('.container')
+  if(parseInt(userNumber) > 0){
+      for(let i=0; i<parseInt(userNumber);i++){
+          const range = fillArray()
+          const board = document.createElement('div')
+          board.className = 'board user-board'
+          for(let i=0; i<24; i++){
+              const random = getRandomNum(range)
+              board.innerHTML += `<div class='cell'>${random}</div>`
+          }
+          container.appendChild(board)
+      }
+  }
+}
 
 window.onload = function(){
-    let bingoCard = document.getElementById('bingo')
-    for(let i=1; i<=76; i++){
-        let cell = document.createElement('div')
-        cell.classList.add('numbers')
-        let numbers = document.createElement('h3')
-        numbers.innerText = [i]
-        cell.appendChild(numbers)
-        bingoCard.appendChild(cell)
-        const range = fillArray()
-
-        }       
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = function(){
-//     let randomArray = []
-//     let count = 0
-//     // let randomNumbers = Math.ceil(Math.random()*76)
-//     let bingoCard = document.getElementById('bingo')
-//     for(let i=0; i<75; i++){
-//         randomArray[i] = randomNumber()
-//         count = randomArray[i]
-//         // let count = randomNumbers + i
-//         let cell = document.createElement('div')
-//         cell.classList.add('numbers')
-//         let numbers = document.createElement('h3')
-
-//         for(let j=0; j<randomArray.length; j++){                   
-//             numbers.innerText = count
-//             cell.appendChild(numbers)
-//             bingoCard.appendChild(cell)
-//         }
-        
-        
-
-        // for (var i = 0; i < 20; i++) {
-        //     var count = 0;
-        //     for (var j = 0; j < Math.floor(Math.random() * 20); j++) {
-        //         count++;
-        //     }
-        //     console.log(count);
-        
-    
+  generateMainBoard()
+  const randBtn = document.getElementById('randButton')
+  const range = fillArray()
+  randBtn.addEventListener('click', function(){
+      generateRandNumber(range)
+  })
+  const userBoardBtn = document.getElementById('user-board-button')
+  userBoardBtn.addEventListener('click', function(){
+      generateUserBoards()
+  })
+}
